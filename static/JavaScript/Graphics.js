475900c4ab04ab83d952e15f6517f24b7e96cc6b
Kyle Hutchinson <kyle.hutchinson@ncr.com>
2019-04-18 12:23:40 -0400

    Made it my own

--- static/JavaScript/Graphics.js
+++ static/JavaScript/Graphics.js
@@ -2,9 +2,13 @@ function Graphics() {
     this.canvas = document.getElementById('canvas');
     this.ctx = canvas.getContext('2d');
 
+    // this.size = 0;
+
     this.updateCanvasSize = () => {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+
+        this.size = Math.min(window.innerWidth, window.innerHeigh);
     };
     this.updateCanvasSize();
 
@@ -18,6 +22,8 @@ function Graphics() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         // Draw things
+        this.ctx.fillStyle = 'white';
+        this.ctx.fillRect(0, 0, 100, 100);
     };
 
     this.startAnimating = () => {
