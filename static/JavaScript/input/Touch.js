a53f05f93952b7d0baf95b3fed697bf182cc12fa
BachNewton <kyle1235@gmail.com>
2019-04-19 08:59:35 -0400

    Added notes for myself

--- static/JavaScript/input/Touch.js
+++ static/JavaScript/input/Touch.js
@@ -10,6 +10,9 @@ function Touch() {
     });
 
     document.addEventListener('touchend', (e) => {
+        // TODO - This needs works
+        // TODO - touchend has a list of the remaining touches, not a list of the ended touches
+
         if (e.touches.length > 0) {
             for (var touch of e.touches) {
                 var id = touch.identifier;
